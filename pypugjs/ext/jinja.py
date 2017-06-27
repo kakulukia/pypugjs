7845e6be9c146595e410babdc02f27903fda9812
Matin Tamizi <mtamizi@gmail.com>
2017-06-27 12:58:55 -0700

    pep8 + improved readability

--- pypugjs/ext/jinja.py
+++ pypugjs/ext/jinja.py
@@ -1,6 +1,6 @@
-from jinja2.ext import Extension
-import os
 import pypugjs.runtime
+import os
+from jinja2.ext import Extension
 
 from pypugjs import Compiler as _Compiler
 from pypugjs.runtime import attrs as _attrs, iteration
@@ -8,69 +8,78 @@ from jinja2 import Markup
 from jinja2.runtime import Undefined
 from pypugjs.utils import process
 
+
 ATTRS_FUNC = '__pypugjs_attrs'
 ITER_FUNC = '__pypugjs_iter'
+
 
 def attrs(attrs, terse=False):
     return Markup(_attrs(attrs, terse, Undefined))
 
+
 class Compiler(_Compiler):
 
-    def visitCodeBlock(self,block):
-        if self.mixing > 0:
-          if self.mixing > 1:
-            caller_name = '__pypugjs_caller_%d' % self.mixing
-          else:
-            caller_name = 'caller'
-          self.buffer('{%% if %s %%}%s %s() %s{%% endif %%}' % (caller_name, self.variable_start_string,
-              caller_name, self.variable_end_string))
-        else:
-          self.buffer('{%% block %s %%}'%block.name)
-          if block.mode=='append': self.buffer('%ssuper()%s' % (self.variable_start_string, self.variable_end_string))
-          self.visitBlock(block)
-          if block.mode=='prepend': self.buffer('%ssuper()%s' % (self.variable_start_string, self.variable_end_string))
-          self.buffer('{% endblock %}')
+    def _wrap_var(self, var):
+        return f'{self.variable_start_string}{var}{self.variable_end_string}'
 
-    def visitMixin(self,mixin):
+    def visitCodeBlock(self, block):
+        if self.mixing > 0:
+            if self.mixing > 1:
+                caller_name = f'__pypugjs_caller_{self.mixing}'
+            else:
+                caller_name = 'caller'
+            self.buffer(
+                f'{{% if {caller_name} %}}'
+                f'{self._wrap_var(caller_name + "()")}'
+                f'{{% endif %}}')
+        else:
+            self.buffer(f'{{% block {block.name} %}}')
+            if block.mode == 'append':
+                self.buffer(self._wrap_var('super()'))
+            self.visitBlock(block)
+            if block.mode == 'prepend':
+                self.buffer(self._wrap_var('super()'))
+            self.buffer('{% endblock %}')
+
+    def visitMixin(self, mixin):
         self.mixing += 1
         if not mixin.call:
-          self.buffer('{%% macro %s(%s) %%}'%(mixin.name,mixin.args))
-          self.visitBlock(mixin.block)
-          self.buffer('{% endmacro %}')
+            self.buffer(f'{{% macro {mixin.name}({mixin.args}) %}}')
+            self.visitBlock(mixin.block)
+            self.buffer('{% endmacro %}')
         elif mixin.block:
-          if self.mixing > 1:
-            self.buffer('{%% set __pypugjs_caller_%d=caller %%}' % self.mixing)
-          self.buffer('{%% call %s(%s) %%}'%(mixin.name,mixin.args))
-          self.visitBlock(mixin.block)
-          self.buffer('{% endcall %}')
+            if self.mixing > 1:
+                self.buffer(
+                    f'{{% set __pypugjs_caller_{self.mixing}=caller %}}')
+            self.buffer(f'{{% call {mixin.name}({mixin.args}) %}}')
+            self.visitBlock(mixin.block)
+            self.buffer('{% endcall %}')
         else:
-          self.buffer('%s%s(%s)%s' % (self.variable_start_string, mixin.name, mixin.args, self.variable_end_string))
+            self.buffer(self._wrap_var(f'{mixin.name}({mixin.args})'))
         self.mixing -= 1
 
-    def visitAssignment(self,assignment):
-        self.buffer('{%% set %s = %s %%}'%(assignment.name,assignment.val))
+    def visitAssignment(self, assignment):
+        self.buffer(f'{{% set {assignment.name} = {assignment.val} %}}')
 
-    def visitCode(self,code):
+    def visitCode(self, code):
         if code.buffer:
             val = code.val.lstrip()
             val = self.var_processor(val)
-            self.buf.append('%s%s%s%s' % (self.variable_start_string, val,'|escape' if code.escape else '',
-                self.variable_end_string))
+            self.buf.append(
+                self._wrap_var(f'{val}{"|escape" if code.escape else ""}'))
         else:
-            self.buf.append('{%% %s %%}'%code.val)
+            self.buf.append(f'{{% {code.val} %}}')
 
         if code.block:
-            # if not code.buffer: self.buf.append('{')
             self.visit(code.block)
-            # if not code.buffer: self.buf.append('}')
-
             if not code.buffer:
-              codeTag = code.val.strip().split(' ',1)[0]
-              if codeTag in self.autocloseCode:
-                  self.buf.append('{%% end%s %%}'%codeTag)
+                code_tag = code.val.strip().split(' ', 1)[0]
+                if code_tag in self.autocloseCode:
+                    self.buf.append(f'{{% end{code_tag} %}}')
 
-    def visitEach(self,each):
-        self.buf.append("{%% for %s in %s(%s,%d) %%}"%(','.join(each.keys),ITER_FUNC,each.obj,len(each.keys)))
+    def visitEach(self, each):
+        self.buf.append(f'{{% for {",".join(each.keys)} in '
+                        f'{ITER_FUNC}({each.obj}, {len(each.keys)}) %}}')
         self.visit(each.block)
         self.buf.append('{% endfor %}')
 
@@ -84,55 +93,37 @@ class Compiler(_Compiler):
                 break
         if not fullpath:
             raise FileNotFoundError(
-                'No such file {} in Jinja loader searchpath'.format(path))
+                f'No such file {path} in Jinja loader searchpath')
         with open(fullpath) as f:
             src = f.read()
         parser = pypugjs.parser.Parser(src)
         block = parser.parse()
         self.visit(block)
 
-    def attributes(self,attrs):
-        return "%s%s(%s)%s" % (self.variable_start_string, ATTRS_FUNC,attrs, self.variable_end_string)
+    def attributes(self, attrs_):
+        return self._wrap_var(f'{ATTRS_FUNC}({attrs_})')
 
 
 class PyPugJSExtension(Extension):
-
-    # def exception_handler(self,pt):
-    #     # print '******************************'
-    #     # print pt.exc_type
-    #     # print pt.exc_value
-    #     # print pt.frames[0].tb
-    #     # line = pt.frames[0].tb.tb_lineno
-    #     # pt.frames[0].tb.tb_lineno = line+10
-
-    #     # print '******************************'
-    #     _,_,tb = fake_exc_info((pt.exc_type,pt.exc_value, pt.frames[0].tb),'asfdasfdasdf',7)
-    #     # pt.frames = [tb]
-    #     raise pt.exc_type, pt.exc_value, tb
     options = {}
     file_extensions = '.pug'
+
     def __init__(self, environment):
-        super(PyPugJSExtension, self).__init__(environment)
-
-        environment.extend(
-            pypugjs=self,
-            # pugjs_env=JinjaEnvironment(),
-        )
-
-        # environment.exception_handler = self.exception_handler
-        # get_corresponding_lineno
+        super().__init__(environment)
+        environment.extend(pypugjs=self)
         environment.globals[ATTRS_FUNC] = attrs
         environment.globals[ITER_FUNC] = iteration
         self.variable_start_string = environment.variable_start_string
         self.variable_end_string = environment.variable_end_string
-        self.options["variable_start_string"] = environment.variable_start_string
-        self.options["variable_end_string"] = environment.variable_end_string
+        self.options['variable_start_string'] = (
+            environment.variable_start_string)
+        self.options['variable_end_string'] = environment.variable_end_string
 
     def preprocess(self, source, name, filename=None):
         if 'include' in source:
             loader = self.environment.loader
             try:
-                # we're in a Flask app
+                # This is necessary in a Flask app
                 loader = loader.app.jinja_loader
             except AttributeError:
                 pass
@@ -141,4 +132,5 @@ class PyPugJSExtension(Extension):
         if (not name or
            (name and not os.path.splitext(name)[1] in self.file_extensions)):
             return source
-        return process(source,filename=name,compiler=Compiler,**self.options)
+        return process(
+            source, filename=name, compiler=Compiler, **self.options)
